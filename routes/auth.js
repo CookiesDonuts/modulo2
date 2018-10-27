65b21788f83181d3401e70635ffe4d57ab2d82c2
Ximena Perez <ximxim25@gmail.com>
2018-10-27 13:24:01 -0500

    config rutas

--- routes/auth.js
+++ routes/auth.js
@@ -4,3 +4,19 @@ const passport = require("passport");
 const User = require("../models/User");
 //const mail = require("../helpers/mailer");
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res) => {
+
+});
+
+router.get("/signup", (req, res) => {
+
+});
+
+router.post("/signup", (req, res) => {
+
+});
+
