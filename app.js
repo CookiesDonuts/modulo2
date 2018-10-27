616dedd9772278497b0aa9a2155895b28f195ccb
Ximena Perez <ximxim25@gmail.com>
2018-10-27 16:57:29 -0500

    login funcionando

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const passport     = require("./helpers/passport");
+const session      = require("express-session");
 
 
 mongoose
@@ -23,6 +25,15 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -47,12 +58,14 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'The paradaise is made from sugar';
 
 
 
 const index = require('./routes/index');
+const auth = require("./routes/auth");
 app.use('/', index);
+app.use("/auth", auth);
 
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,44 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/User");
-//const mail = require("../helpers/mailer");
+const mail = require("../helpers/mailer");
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.post("/login", (req, res) => {
-
-});
+router.post("/login",  passport.authenticate("local", {
+  successRedirect: "/checkorder",
+  failureRedirect: "/auth/login"
+}));
 
 router.get("/signup", (req, res) => {
+  res.render("signup");
 
 });
 
 router.post("/signup", (req, res) => {
-
+  if(req.body.password !== req.body["password-confirm"]) return res.render("signup", {msg: "Las contraseñas no coinciden"});
+  const {username, email, password} = req.body;
+  User.register({username, email}, password)
+      .then(user => {
+          const options = {
+              email: user.email,
+              subject: "Confirmar correo",
+              message: "Por favor confirma tu correo"
+          };
+          mail.send(options);
+          res.redirect("/auth/login")
+      })
+      .catch(err => {
+          res.status(500).render("signup",{err, msg:"No se completó el registro"})
+      })
 });
+
+router.post("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/auth/login");
+});
+
+module.exports = router;
 
