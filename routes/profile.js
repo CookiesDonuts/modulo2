0919cb3eb4a7c6b050600463b3440316d9154f60
Ximena Perez <ximxim25@gmail.com>
2018-11-03 12:54:01 -0600

    checa si hay login de usuario

--- routes/profile.js
+++ routes/profile.js
@@ -15,12 +15,28 @@ function checkIfOwner(req, res, next){
                 req.user = user;
                 return next();
             }
+            console.log("que hay",user)
             res.redirect("/profile");
         })
         .catch(() => {
-            res.redirect("/profile");
+            res.redirect("/");
         });
 }
+
+
+router.get("/", isLoggedIn, (req, res) => {
+
+    res.render("profile",{user: req.user})
+
+   /* solo es una busqueda de todos los usuarios 
+   Users.find()
+        .populate("owner", "username")
+        .then(user => {
+            res.render("profile",{user})<--- mandar tu datos a tus vistas para utilizados 
+            
+        })
+        */
+});
 
 
 
