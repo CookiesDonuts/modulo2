186e41763162e1911441e86df05c9aaa26d399ee
Manuel Navarro <manuel.alejandro.navarro@gmail.com>
2018-11-08 20:10:23 -0600

    navbar, estilos, login, faq

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,19 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  const {user} = req;
+  res.render('index', {user});
 });
+
+router.get("/products", (req, res) => {
+  const {user} = req;
+  res.render("products", {user});
+})
+
+router.get("/contact", (req, res) => {
+    const {user} = req;
+    res.render("contact", {user});
+})
 
 
 module.exports = router;
--- routes/promos.js
+++ routes/promos.js
@@ -3,7 +3,8 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-    res.render('promos');
+    const {user} = req;
+    res.render('promos', {user});
 });
 
 
